0a671d3f410e53aa20ca963d0f1d7effdae00a75
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-09-04 21:40:17 -0300

    Add files via upload

--- Semana_cuatro/action_page.php
+++ Semana_cuatro/action_page.php
@@ -1,7 +1,17 @@
 <?php
 //var_dump($_GET);
 Echo "Su nombre es".$_GET['nombre'];
+
+
+$miobjeto=new stdClass();
+$miobjeto->nombre="alfredo";
+$miobjeto->apellido="mercurio";
+
+//var_dump(json_encode($miobjeto));
 $archivo=fopen('usuarios.txt','a');
-fwrite($archivo, $_GET['nombre'])."\n";
+fwrite($archivo, json_encode($miobjeto));
+fclose($archivo);
+
+
 
 ?>
