2cba29da123db14215d7ecdb3f5f2d7d0e7004f6
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-09-17 20:20:59 -0300

    Add files via upload

---
+++ Semana_Seis/action_page.php
@@ -0,0 +1,10 @@
+<?php
+$miobjeto=new stdClass();
+$miobjeto->nombre=$_GET['nombre'];
+$miobjeto->apellido=$_GET['apellido'];
+
+$archivo=fopen('usuarios.txt','a');
+fwrite($archivo, json_encode($miobjeto)."\n");
+fclose($archivo);
+
+?>
